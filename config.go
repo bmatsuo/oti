8a7a91a61f99858b3a15a1edc64caf66eee6f43b
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-03-20 20:36:36 -0700

    read aws credentials

--- config.go
+++ config.go
@@ -29,3 +29,25 @@ func readConfig(path string, c *OTIConfig) error {
 	}
 	return nil
 }
+
+func (c *OTIConfig) AwsKey() (*AwsKey, error) {
+	// TODO stat and warn if permissions are not strict
+
+	keyp, err := ioutil.ReadFile(c.AwsKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var k AwsKey
+	err = json.Unmarshal(keyp, &k)
+	if err != nil {
+		return nil, err
+	}
+
+	return &k, nil
+}
+
+type AwsKey struct {
+	AccessKey string
+	SecretKey string
+}
--- oti.go
+++ oti.go
@@ -21,40 +21,40 @@ var Config = &OTIConfig{
 	ResourceTag: OTITag{"ManagingAgent", "oti"},
 }
 
-func main() {
-	logger := log.New(os.Stderr, "", 0)
+var Log = log.New(os.Stderr, "", 0)
 
+func main() {
 	fs := flag.NewFlagSet("oti", flag.ExitOnError)
 	fs.StringVar(&ConfigPath, "c", ConfigPath, "config file location")
 	fs.Usage = func() {
-		logger.Println("usage: oti [options] command")
-		logger.Println()
-		logger.Println("options:")
+		Log.Println("usage: oti [options] command")
+		Log.Println()
+		Log.Println("options:")
 		fs.PrintDefaults()
-		logger.Println()
-		logger.Println("commands:")
+		Log.Println()
+		Log.Println("commands:")
 		for _, cmd := range otisub.GetAll() {
-			logger.Print("\t", cmd.Name())
+			Log.Print("\t", cmd.Name())
 		}
-		logger.Println()
-		logger.Println("for details about a specific command:")
-		logger.Println("\toti command -h")
+		Log.Println()
+		Log.Println("for details about a specific command:")
+		Log.Println("\toti command -h")
 	}
 	fs.Parse(os.Args[1:])
 
 	cmdname, args := getargs(fs.Args(), "lifecycle")
 	cmd := otisub.Get(cmdname)
 	if cmd == nil {
-		logger.Printf("no command %q; exiting", cmdname)
-		logger.Fatal("for a list of commands run oti -h")
+		Log.Printf("no command %q; exiting", cmdname)
+		Log.Fatal("for a list of commands run oti -h")
 	}
 
 	err := readConfig(ConfigPath, Config)
 	if err != nil {
 		if os.IsNotExist(err) {
-			logger.Println("warning: config file not found. using defaults.")
+			Log.Println("warning: config file not found. using defaults.")
 		} else {
-			logger.Fatal("error reading config: ", err)
+			Log.Fatal("error reading config: ", err)
 		}
 	}
 
